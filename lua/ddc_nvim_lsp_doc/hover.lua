a1cda71a5b1815647e0dc3504aa9e62991e9684e
matsui54 <haru.matu9168@gmail.com>
2021-08-20 18:48:13 +0900

    Remove codes

--- lua/ddc_nvim_lsp_doc/hover.lua
+++ lua/ddc_nvim_lsp_doc/hover.lua
@@ -12,20 +12,21 @@ end
 local get_resolved_item = function(arg, callback)
   local item = arg[1]
   vim.lsp.buf_request(0, 'completionItem/resolve', item, function(_, _, res)
+    if res then
     -- print(vim.inspect(res))
-    respond(callback, res)
+      respond(callback, res)
+    end
   end)
 end
 
 local get_signature_help = function(_, callback)
   local params = vim.lsp.util.make_position_params()
   vim.lsp.buf_request(0, "textDocument/signatureHelp", params, function(_, _, res)
-    local converted, hl = '', ''
     if res then
       local ft = api.nvim_buf_get_option(0, 'filetype')
-      converted, hl = vim.lsp.util.convert_signature_help_to_markdown_lines(res, ft)
+      local converted, hl = vim.lsp.util.convert_signature_help_to_markdown_lines(res, ft)
+      respond(callback, {help = res, lines = converted, hl = hl})
     end
-    respond(callback, {help = res, lines = converted, hl = hl})
   end)
 end
 
@@ -36,36 +37,8 @@ local close_win = function(win_name)
   end
 end
 
-local show_float_win = function(opts)
-  local floating_bufnr = api.nvim_create_buf(false, true)
-  if floating_bufnr == 0 then return end
-  if opts.syntax == "markdown" then
-    vim.lsp.util.stylize_markdown(floating_bufnr, opts.lines)
-  else
-    api.nvim_buf_set_lines(floating_bufnr, 0, -1, true, opts.lines)
-  end
-
-  -- First, show window, then close remaining window to avoid flicker
-  local floating_winnr = api.nvim_open_win(floating_bufnr, false, opts.floatOpt)
-  close_win(opts.winName)
-
-  api.nvim_buf_set_var(0, opts.winName, floating_winnr)
-  if opts.syntax == "markdown" then
-    api.nvim_win_set_var(floating_winnr, "conceallevel", 2);
-    api.nvim_win_set_var(floating_winnr, "concealcursor", "n")
-  end
-  api.nvim_win_set_var(floating_winnr, "foldenable", false);
-  api.nvim_win_set_var(floating_winnr, "modifiable", false);
-  api.nvim_win_set_var(floating_winnr, "bufhidden", "wipe");
-  vim.lsp.util.close_preview_autocmd(opts.events, floating_winnr)
-  if opts.hl then
-    api.nvim_buf_add_highlight(floating_bufnr, -1, "LspSignatureActiveParameter", 0, unpack(opts.hl))
-  end
-end
-
 return {
   is_lsp_active = is_lsp_active,
-  show_float_win = show_float_win,
   get_resolved_item = get_resolved_item,
   get_signature_help = get_signature_help,
   close_win = close_win,
