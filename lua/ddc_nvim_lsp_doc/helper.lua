ee73b2834337677a551f6817d4d5c1a76a9e9f96
matsui54 <haru.matu9168@gmail.com>
2021-09-06 20:26:03 +0900

    Fix for the nvim's breaking change

--- lua/ddc_nvim_lsp_doc/helper.lua
+++ lua/ddc_nvim_lsp_doc/helper.lua
@@ -1,6 +1,12 @@
 local vim = vim
 local api = vim.api
 
+local is_new_handler = function(arg)
+  -- For neovim 0.6 breaking changes
+  -- https://github.com/neovim/neovim/pull/15504
+  return vim.fn.has('nvim-0.6') and type(arg) == 'table'
+end
+
 local respond = function(type, item)
   api.nvim_call_function('denops#notify', {'ddc_nvim_lsp_doc', 'respond', {type, item}})
             -- `call denops#notify('${denops.name}', 'onEvent',["${event}"])`,
@@ -8,7 +14,8 @@ end
 
 local get_resolved_item = function(arg)
   local item = arg.decoded
-  vim.lsp.buf_request(0, 'completionItem/resolve', item, function(_, _, res)
+  vim.lsp.buf_request(0, 'completionItem/resolve', item, function(_, arg1, arg2)
+    local res = is_new_handler(arg1) and arg1 or arg2
     if res then
     -- print(vim.inspect(res))
       respond("doc", {item = res, selected = arg.selected})
@@ -18,7 +25,8 @@ end
 
 local get_signature_help = function(arg)
   local params = vim.lsp.util.make_position_params()
-  vim.lsp.buf_request(0, "textDocument/signatureHelp", params, function(_, _, res)
+  vim.lsp.buf_request(0, "textDocument/signatureHelp", params, function(_, arg1, arg2)
+    local res = is_new_handler(arg1) and arg1 or arg2
     if res and res ~= 0 and res.signatures and res.signatures[1] then
       local ft = api.nvim_buf_get_option(0, 'filetype')
       local converted, hl = vim.lsp.util.convert_signature_help_to_markdown_lines(res, ft)
