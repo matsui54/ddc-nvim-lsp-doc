02c30cb5c2da2b19c989abb64d5c92d858595413
matsui54 <haru.matu9168@gmail.com>
2021-09-19 12:59:04 +0900

    Update deps

--- denops/ddc_nvim_lsp_doc/deps.ts
+++ denops/ddc_nvim_lsp_doc/deps.ts
@@ -1,12 +1,12 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.9.0/mod.ts#^";
+export type { Denops } from "https://deno.land/x/denops_std@v1.11.2/mod.ts#^";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v1.9.0/batch/mod.ts#^";
-export * as op from "https://deno.land/x/denops_std@v1.9.0/option/mod.ts#^";
-export * as fn from "https://deno.land/x/denops_std@v1.9.0/function/mod.ts#^";
-export * as nvimFn from "https://deno.land/x/denops_std@v1.9.0/function/nvim/mod.ts#^";
-export * as vars from "https://deno.land/x/denops_std@v1.9.0/variable/mod.ts#^";
-export * as autocmd from "https://deno.land/x/denops_std@v1.9.0/autocmd/mod.ts#^";
-export { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts#^";
-export * as log from "https://deno.land/std@0.106.0/log/mod.ts#^";
+} from "https://deno.land/x/denops_std@v1.11.2/batch/mod.ts#^";
+export * as op from "https://deno.land/x/denops_std@v1.11.2/option/mod.ts#^";
+export * as fn from "https://deno.land/x/denops_std@v1.11.2/function/mod.ts#^";
+export * as nvimFn from "https://deno.land/x/denops_std@v1.11.2/function/nvim/mod.ts#^";
+export * as vars from "https://deno.land/x/denops_std@v1.11.2/variable/mod.ts#^";
+export * as autocmd from "https://deno.land/x/denops_std@v1.11.2/autocmd/mod.ts#^";
+export { assertEquals } from "https://deno.land/std@0.107.0/testing/asserts.ts#^";
+export * as log from "https://deno.land/std@0.107.0/log/mod.ts#^";
